15b9fe026c59fd942e94a2ba7f34d9db9c805ddb
Elena <lenhenlady@gmail.com>
2021-02-04 12:49:48 +0300

    second review fix

--- backend/middlewares/celebrate-validator.js
+++ backend/middlewares/celebrate-validator.js
@@ -57,7 +57,7 @@ const addLikeValidator = celebrate({
 
 const delLikeValidator = celebrate({
   body: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
+    id: Joi.string().hex().length(24),
   }),
 });
 
